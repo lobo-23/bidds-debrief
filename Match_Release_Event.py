6b486e4f0f8628603cb54798a9693e334982c01c
Brent Drabek <drabekb@gmail.com>
2020-04-22 01:05:41 -0500

    Add JdamParse function Spaces up to 5 now for Gravity Values

--- Match_Release_Event.py
+++ Match_Release_Event.py
@@ -4,12 +4,20 @@ def msnEventparse(filename):
     input = pd.read_csv(filename)
     records = pd.read_csv('test_records.csv')
 
-    input['Date'] = pd.to_datetime(input['Date'] + ' ' + input['Time (UTC)'])
-    records['Date'] = pd.to_datetime(records['Date'] + ' ' + records['Time (UTC)'])
+    input['Time (UTC)'] = pd.to_datetime(input['Date'] + ' ' + input['Time (UTC)'])
+    records['Time (UTC)'] = pd.to_datetime(records['Date'] + ' ' + records['Time (UTC)'])
 
-    msnEvents = [pd.Index(records['Date']).get_loc(x, method='nearest') for x in input['Date']]
-    input.insert(1, "dataEvent", msnEvents, True)
-    print(input[['Record Number','dataEvent','Time (UTC)']])
+    msnEventsIndex = [pd.Index(records['Time (UTC)']).get_loc(x, method='nearest') for x in input['Time (UTC)']]
+    msnEvent = [records['Time (UTC)'].loc[x] for x in msnEventsIndex]
+    recordNumber = [records['Record Number'].loc[x] for x in msnEventsIndex]
+
+    input.insert(6, "msnEventTime", msnEvent, True)
+    input.insert(7, "RecordNumber", recordNumber, True)
+    input.insert(8, "msnEventIndex", msnEventsIndex, True)
+    #print(input[['Record Number','msnEventIndex','Time (UTC)']])
+    print(msnEvent)
+    fileout = str(filename)[1][:-3] + '_expanded.csv'
+    input.to_csv(fileout,index=False)
 
 
-#msnEventparse('test_jdam.csv')
+msnEventparse('test_jdam.csv')
--- Parse_Release.py
+++ Parse_Release.py
@@ -1,7 +1,112 @@
 from pyparsing import *
+import pandas as pd
 import csv
 
 debug = False
+
+def JdamParse(wpn):
+    wpn['Tail'] = wpn['Tail Year'][1] + '0' + wpn['Tail Number']
+    # print(wpn['Tail Number'])
+
+    if wpn['MSN Planned TGT'] == 'True':
+        wpn['Dest'] = 'MPdest'
+    else:
+        if wpn['LP DT Num'].isnumeric():
+            wpn['Dest'] = "D" + str(wpn['LP DT Num'])
+        else:
+            wpn['Dest'] = "DS" + wpn['Mode'].replace('Static', 'STAT').replace('Continuous', 'CONT')
+
+    # print(wpn['Dest'])
+
+    wpn['TOR'] = pd.to_datetime(wpn['Time (UTC)'] + ' ' + wpn['Date'])
+    # print(wpn['TOR'])
+
+    if wpn['IZ Status'] == 'Inside':
+        wpn['LARstatus'] = 'ZONE'
+        wpn['TOF'] = wpn['IZ TOF']
+    elif wpn['IR Status'] == 'RANGE':
+        wpn['LARstatus'] = 'IR'
+        wpn['TOF'] = wpn['IR TOF']
+    else:
+        wpn['LARstatus'] = 'UNK'
+        wpn['TOF'] = 0
+
+    if wpn['TOF'] != 0:
+        try:
+            wpn['TOF'] = float(wpn['TOF'].replace('  sec', '').replace('+ ', ''))
+        except:
+            print('TOF Parse Error- ' + wpn['TOF'])
+
+    wpn['TOT'] = wpn['TOR'] + pd.to_timedelta(str(wpn['TOF']) + 's')
+    # print(wpn['TOR'])
+    # print(wpn['TOF'])
+    # print(wpn['TOT'])
+
+    # print(wpn['WPN Type'])
+
+    try:
+        wpn['ALT'] = str(round(float(wpn['Altitude'].replace('  feet', '').replace('+ ', '')))) + "'"
+    except:
+        wpn['ALT'] = 'ERR'
+    #print(wpn['ALT'])
+
+    try:
+        wpn['LAT'] = wpn['Latitude'].replace('  deg', '').replace(':', ' ')
+    except:
+        wpn['LAT'] = 'ERR'
+    try:
+        wpn['LONG'] = wpn['Longitude'].replace('  deg', '').replace(':', ' ')
+    except:
+        wpn['LONG'] = 'ERR'
+
+    # print(wpn['LAT'])
+    # print(wpn['LONG'])
+
+    try:
+        wpn['TGT LAT'] = wpn['TGT LAT'].replace('  deg', '').replace(':', ' ')
+    except:
+        wpn['TGT LAT'] = 'ERR'
+    try:
+        wpn['TGT LONG'] = wpn['TGT LONG'].replace('  deg', '').replace(':', ' ')
+    except:
+        wpn['TGT LONG'] = 'ERR'
+
+    # print(wpn['TGT LAT'])
+    # print(wpn['TGT LONG'])
+    try:
+        wpn['TGT ELEV'] = float(wpn['TGT Altitude'].replace('  meters', '').replace('+ ', '')) * 3.28084
+        wpn['TGT ELEV'] = str(round(wpn['TGT ELEV'])) + "' " + wpn['TGT Alt Ref']
+    except:
+        wpn['TGT LAT'] = 'ERR'
+
+    # print(wpn['TGT ELEV'])
+
+    try:
+        wpn['GTRK'] = round(float(wpn['GND Trk Angle'].replace('+ ', '').replace('- ', '').replace('  deg', '')))
+    except:
+        wpn['GTRK'] = 'ERR'
+    # print(wpn['GTRK'])
+
+    wpn['LS'] = wpn['Dev ID'].replace('P', '')
+    # print(wpn['LS'])
+
+    wpn['GS'] = round(float(wpn['GND Speed'].replace('  ft/sec', '')) * 0.592484)
+    # print(wpn['GS'])
+
+    wpn['PrimeNav'] = wpn['Prime Nav System']
+    # print(wpn['PrimeNav'])
+
+    if wpn['Func at Impact'] == 'TRUE':
+        wpn['Delay'] = "IMP"
+    elif wpn['Func on Proximity'] == 'TRUE':
+        wpn['Delay'] = 'PROX'
+    elif wpn['Func on Time Aft Impact'] == 'TRUE':
+        wpn['Delay'] = 'DEL'
+    elif wpn['Func on Void'] == 'TRUE':
+        wpn['Delay'] = 'VOID' + str(wpn['Void Number'])
+
+    return wpn
+
 
 sample = open('Test 1012.txt', 'r', errors='ignore').read()
 
@@ -17,7 +122,7 @@ recordBlock = record + OneOrMore(eventData) + msnEventExpanded
 pData = Suppress(Literal("PERTINENT DATA"))
 SPACE_CHARS = ' \t'
 dataField = CharsNotIn(SPACE_CHARS)
-space = Word(SPACE_CHARS, exact=1)^Word(SPACE_CHARS, exact=2)^Word(SPACE_CHARS, exact=3)^Word(SPACE_CHARS, exact=4)
+space = Word(SPACE_CHARS, exact=1)^Word(SPACE_CHARS, exact=2)^Word(SPACE_CHARS, exact=3)^Word(SPACE_CHARS, exact=4)^Word(SPACE_CHARS, exact=5)
 dataKey = delimitedList(dataField, delim=space, combine=True)
 dataValue = Combine(dataField + ZeroOrMore(space + dataField))
 
@@ -31,7 +136,9 @@ gcount = 0
 
 if debug:
     for obj, start, end in name_parser.scanString(sample):
-        print(obj.dump())
+        if obj['Application ID'] == '13':
+            JdamParse(obj)
+        print(obj.asDict())
         count += 1
 else:
     with open('test_gravity.csv', 'w+', newline='') as gravity:
@@ -41,6 +148,7 @@ else:
 
             for obj, start, end in name_parser.scanString(sample):
                 if obj['Application ID'] == '13':
+                    JdamParse(obj)
                     if jcount == 0:
                         writerjdam.writerow(obj.asDict().keys())
                     input = list(obj.asDict().values())
